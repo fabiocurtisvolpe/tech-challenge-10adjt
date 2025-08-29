2c7eba01a55e94226a3389c0c9e8eeea392672b6
Fabio Curtis Volpe <curtis.fabio@outlook.com>
2025-08-29 11:10:35 -0300

    configuração inicial projeto

---
+++ adjt/src/main/java/com/postech/adjt/AdjtApplication.java
@@ -0,0 +1,13 @@
+package com.postech.adjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjtApplication.class, args);
+	}
+
+}
